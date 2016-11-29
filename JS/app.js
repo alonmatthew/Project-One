298676a95c8b2b5117012b7a8bb687ecf2e6af91
Matthew Alon <alonmatthew94@gmail.com>
2016-11-29 14:24:39 -0800

    created logic to move catcher left and right & logic to remove falling boxes

--- JS/app.js
+++ JS/app.js
@@ -2,12 +2,16 @@ var container1 = $('#container1');
 var container2 = $('#container2');
 var container3 = $('#container3');
 var containers = [container1, container2, container3];
+var main = $('#mainContainer');
+var catcher = $('#catcher');
+
+$(container2).append(catcher);
 
 function genRandomNum(min, max) {
   return Math.round((Math.random() * (max-min)) + min);
 }
 
-$('button').on('click', function() {
+$('.start').on('click', function() {
   setInterval(function() {
     new Box();
   },2000);
@@ -19,6 +23,18 @@ function Box() {
   containers[genRandomNum(0,3)].prepend($newDiv);
 
   setInterval(function() {
-      $newDiv.animate({top: "+=30"},100);
+    if ($newDiv.offset().top > 700) {
+      $newDiv.remove();
+    } else {
+      $newDiv.animate({top: "+=30"},125);
+    }
   });
 }
+
+$('.moveLeft').on('click', function() {
+  catcher.animate({left: "-=100"});
+});
+
+$('.moveRight').on('click', function() {
+  catcher.animate({left: "+=100"});
+});
